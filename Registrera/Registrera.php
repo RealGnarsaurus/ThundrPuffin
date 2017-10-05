173c8518299ce02220e686e84e6a1806d96e7826
MikaelP <mikael.paavilainen@elev.it-gymnasiet.se>
2017-10-05 14:31:35 +0200

    Working Register page with error msg.

---
+++ Registrera/Registrera.php
@@ -0,0 +1,50 @@
+<?php
+session_start();
+
+  require('Helpers/db.php');
+
+ ?>
+ <!DOCTYPE html>
+ <html>
+ <script src="Helpers/IP.js"></script><!--LocalIp-->
+ <?php
+ $IP;
+  if (!empty($_SESSION['errorMsg'])) {
+    ?>
+    <h2><?php echo $_SESSION['errorMsg'];?></h2>
+    <?php
+  }
+  ?>
+
+ <body onload="GetLocalIp();">
+  <form action="Helpers/Reg_Check.php" method="post">
+    Username:
+    <br>
+    <input type="text" name="username" value="">
+    <br>
+    Password:
+    <br>
+    <input type="text" name="password" value="">
+    <br>
+    Repeat Password:
+    <br>
+    <input type="text" name="password2" value="">
+    <br>
+    Email:
+    <br>
+    <input type="text" name="email" value="">
+    <input id="Public" type="text" name="localIP" value="" hidden>
+    <input id="Local" type="text" name="publicIP" value="" hidden>
+    <script type="application/javascript">
+      function getIP(json) {
+        document.getElementById("Public").value=json.ip; //
+        //document.write("My public IP address is: ", json.ip);
+      }
+    </script>
+    <script type="application/javascript" src="https://api.ipify.org?format=jsonp&callback=getIP"></script> <!--Public Ip-->
+    <br>
+    <input type="submit" value="Sign Up">
+  </form>
+
+ </body>
+ </html>
