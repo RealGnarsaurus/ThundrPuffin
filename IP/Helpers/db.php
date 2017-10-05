81dec1482c19526556d1a414af0dbc51f7324703
MikaelP <mikael.paavilainen@elev.it-gymnasiet.se>
2017-10-05 13:15:25 +0200

    deaded

--- IP/Helpers/db.php
+++
@@ -1,9 +0,0 @@
-<!--Connects to database for info-->
-<?php
-$dsn = "mysql:host=localhost;dbname=blogg"; //Server Auth
-$login = "root";//Username
-$password = "";//Password
-$options = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"); //Set Value as UT8 Coding
-$dbh = new pdo($dsn,$login,$password,$options); //Establish Con..
-$dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-?>
