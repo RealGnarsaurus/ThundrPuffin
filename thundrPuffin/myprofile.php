a309f1cba36b4c2a4a66a2bfe0f4c7421ea86daf
Robin Höglund <32061606+CloudZebra@users.noreply.github.com>
2017-10-24 10:36:13 +0200

    Layout på myprofile och manageblogg är fixad

--- thundrPuffin/myprofile.php
+++ thundrPuffin/myprofile.php
@@ -19,6 +19,7 @@ if (!empty($_SESSION['userID'])) {
 
     foreach ($result as $res) {
   ?>
+    <h1>User Info</h1>
     <h2>Username</h2><?php
     echo $res->Username;
     ?>
@@ -30,30 +31,27 @@ if (!empty($_SESSION['userID'])) {
       ?>
       <img id="previewImg" src="../Blogg/Images/Avatar<?php echo $userID;?>.png" style="background-color:black;width:100px;height:auto;">
       <?php
-      echo "File exist!";
     }
     else{
       ?>
       <img id="previewImg" src="../Blogg/Images/Stock.png" style="background-color:black;width:100px;height:auto;">
       <?php
-      echo "../Blogg/Images/Avatar{$userID}.png";
-      echo "File does not exist!";
     }
     ?>
   </div>
   
   <div id="profileChanges">
-    <h2>Edit profile</h2>
+    <h1>Edit profile</h1>
     <form id="MyForm" action="../Helpers/MyProfile_Check.php" method="post"enctype="multipart/form-data">
-    <h3>Change Avatar</h3>
+    <h2>Change Avatar</h2>
     <input type="file" name="Avatar" onchange="preview(event,'previewImg');" id="BG">
-    <h3>New Email</h3>
+    <h2>New Email</h2>
     <input type="text" name="Email" placeholder="<?php echo $res->Email;?>">
-    <h3>New Password</h3>
+    <h2>New Password</h2>
     <input type="password" name="Password" placeholder="">
-    <h3>Repeat New Password</h3>
+    <h2>Repeat New Password</h2>
     <input type="password" name="Password2" placeholder="">
-    <h3>Confirm With Old Password</h3>
+    <h2>Confirm With Old Password</h2>
     <input type="password" name="CurrentPassword" placeholder="" required>
     <input type="password" name="userID" value="<?php echo $userID;?>" placeholder="" hidden>
     </br>
