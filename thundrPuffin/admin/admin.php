40c23ebf437a10ca3cc6c1988a7a97b0f3c909ce
Robin Höglund <32061606+CloudZebra@users.noreply.github.com>
2017-10-25 08:29:36 +0200

    Behövde använda mina backup filer

--- thundrPuffin/admin/admin.php
+++ thundrPuffin/admin/admin.php
@@ -1,6 +1,6 @@
 <?PHP
 	session_start();
-    require('../helpers/db.php');
+    require ('../Helpers/db.php');
 
     $userID = $_SESSION['userID'];
     //Gets Blogg ID
@@ -26,31 +26,32 @@
 		<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-		<script src="../helpers/getcontent.js"></script>
-		<script src="../helpers/preview.js"></script>
-		<script src="../helpers/editstyle.js"></script>
-		<script src="../helpers/updateinstant.js"></script>
+		<script src="../Helpers/getContent.js"></script>
+		<script src="../Helpers/preview.js"></script>
+		<script src="../Helpers/EditStyle.js"></script>
+		<script src="../Helpers/UpdateInstant.js"></script>
 		<link rel="stylesheet" type="text/css" href="../css/main.css">
-		<link rel="stylesheet" type="text/css" href="../css/adminstyle.css">
+		<link rel="stylesheet" type="text/css" href="../css/adminStyle.css">
 	</head>
 	<body>
 		<div id="adminSideNav" class="sidenav">
             <br/>
 						<?php
-						if (file_exists("../blogg/images/avatar$userID.png")) {
+						if (file_exists("../Blogg/Images/Avatar$userID.png")) {
 							?>
-							<img class="avatar img-circle" src="../blogg/images/avatar<?php echo $userID;?>.png" style="height: 8vw; width: auto; display: block;">
+							<img class="avatar img-circle" src="../Blogg/Images/Avatar<?php echo $userID;?>.png" style="height: 8vw; width: auto; display: block;">
 							<?php
 						}
 						else{
 							?>
-							<img class="avatar img-circle" src="../blogg/images/stock.png" style="height: 8vw; width: auto; display: block;">
+							<img class="avatar img-circle" src="../Blogg/Images/Stock.png" style="height: 8vw; width: auto; display: block;">
 							<?php
 						}
 						?>
             <p><?php echo $result2[0]->Username;?></p>
             <br/>
 						<button href="#" class="adminLiButton btn" id="manageUser" onClick="MyProfile()"><span class="material-icons">assignment_ind </span> My Profile</button>
+						<button href="#" class="adminLiButton btn" id="customizeBlog" onClick="blogSettings()"><span class="material-icons">settings </span> Customize blog</button>
 						<?php
 							$sql3 = "SELECT * FROM blogg where UserID='$userID'"; //Check if user has blogg
 							//echo $sql;
@@ -59,17 +60,11 @@
 							$result3 = $stmt3->fetchAll();
 							if (!empty($result3)) {
 						 ?>
-						 	<button href="#" class="adminLiButton btn" id="customizeBlog" onClick="blogSettings()"><span class="material-icons">settings </span> Customize blog</button>
 							<button href="#" class="adminLiButton btn" id="manageUser" onClick="loadDoc()"><span class="material-icons">assignment_ind </span> Manage Users</button>
 							<button href="#" class="adminLiButton btn" id="flagReports" onClick="showFlagReports()"><span class="material-icons">feedback</span>Flag reports</button>
-							</br>
-							<button href="#" class="adminLiButton btn" onClick="window.location.href='../blogg/sf/index.php?bloggID=<?PHP echo $result[0]->ID;?>'"><span class="material-icons">desktop_windows </span> Go to blog</button>
+						</br>
+						<button href="#" class="adminLiButton btn" onClick="window.location.href='../Blogg/sf/index.php?bloggID=<?PHP echo $result[0]->ID;?>'"><span class="material-icons">desktop_windows </span> Go to blog</button>
 						<?php
-						}
-						else{
-							?>
-							<button href="#" class="adminLiButton btn" id="customizeBlog" onClick="blogSettings()"><span class="material-icons">settings </span> Create blog</button>
-							<?php
 						}
 						 ?>
 				</div>
@@ -82,11 +77,11 @@
 					<ul class="nav navbar-nav">
 						<li><a href="../welcome.php">Home</a></li>
 						<li><a href="../browsbloggs.php">Browse blogs</a></li>
-						<li><a href="#">Features(W)</a></li>
-						<li><a href="#">About(W)</a></li>
+						<li><a href="#">Features</a></li>
+						<li><a href="#">About</a></li>
 					</ul>
 					<ul class="nav navbar-nav navbar-right" id="logOut">
-						<li><a href="../helpers/logout.php">Logout</a></li>
+						<li><a href="../Helpers/Logout.php">Logout</a></li>
 					</ul>
 				</div>
 			</nav>
--- thundrPuffin/myprofile.php
+++ thundrPuffin/myprofile.php
@@ -1,58 +1,63 @@
 <?php
 session_start();
-require("helpers/db.php");
+require("Helpers/db.php");
 if (!empty($_SESSION['userID'])) {
   $userID = $_SESSION['userID'];
 }
-$sql = "SELECT * from userinfo WHERE ID ='$userID'";
-//echo $sql;
-$stmt = $dbh->prepare($sql);
-$stmt->execute();
-$result = $stmt->fetchAll();
+?>
 
-foreach ($result as $res) {
-  ?>
-  <h2>Username</h2><?php
-  echo $res->Username;
-  ?>
-  <h2>Email</h2><?php
-  echo $res->Email;
-  ?>
-  <h2>Avatar</h2><?php
-  //echo "../Blogg/Images/Avatar$userID.png";
-  if (!file_exists("../blogg/images/avatar$userID.png")) {
-    //echo "files Exist";
-      ?>
-      <img id="previewImg" src="../blogg/images/avatar<?php echo $userID;?>.png" style="background-color:black;width:100px;height:auto;">
-      <?php
-  }
-  else{
-    ?>
-    <img id="previewImg" src="../blogg/images/stock.png" style="background-color:black;width:100px;height:auto;">
-    <?php
-  }
-  ?>
-
-  <!DOCTYPE html>
-  <script src="../helpers/preview.js"></script>
-  <html>
+<!DOCTYPE html>
+<script src="../Helpers/preview.js"></script>
+<html>
  <body>
-   <h2>New Information</h2>
-   <form id="MyForm" action="../helpers/myprofile_check.php" method="post"enctype="multipart/form-data">
-     Avatar:<input type="file" name="Avatar" onchange="preview(event,'previewImg');" id="BG">
-     <br>
-     Email:<input type="email" name="Email" placeholder="<?php echo $res->Email;?>">
-     <br>
-     Password:<input type="password" name="Password" placeholder="">
-     <br>
-     Repeat Password:<input type="password" name="Password2" placeholder="">
-     <br>
-     Confirme With Current Password<input type="password" name="CurrentPassword" placeholder="" required>
-     <br>
-     <input type="password" name="userID" value="<?php echo $userID;?>" placeholder="" hidden>
-     <input type="submit" name="" value="Update">
-   </form>
+<div id="profileInformation">
+  <?php
+    $sql = "SELECT * from userinfo WHERE ID ='$userID'";
+    $stmt = $dbh->prepare($sql);
+    $stmt->execute();
+    $result = $stmt->fetchAll();
 
+    foreach ($result as $res) {
+  ?>
+    <h1>User Info</h1>
+    <h2>Username</h2><?php
+    echo $res->Username;
+    ?>
+    <h2>Email</h2><?php
+    echo $res->Email;
+    ?>
+    <h2>Avatar</h2><?php
+    if (!file_exists("../Blogg/Images/Avatar{$userID}.png")) {
+      ?>
+      <img id="previewImg" src="../Blogg/Images/Avatar<?php echo $userID;?>.png" style="background-color:black;width:100px;height:auto;">
+      <?php
+    }
+    else{
+      ?>
+      <img id="previewImg" src="../Blogg/Images/Stock.png" style="background-color:black;width:100px;height:auto;">
+      <?php
+    }
+    ?>
+  </div>
+  
+  <div id="profileChanges">
+    <h1>Edit profile</h1>
+    <form id="MyForm" action="../Helpers/MyProfile_Check.php" method="post"enctype="multipart/form-data">
+    <h2>Change Avatar</h2>
+    <input type="file" name="Avatar" onchange="preview(event,'previewImg');" id="BG">
+    <h2>New Email</h2>
+    <input type="text" name="Email" placeholder="<?php echo $res->Email;?>">
+    <h2>New Password</h2>
+    <input type="password" name="Password" placeholder="">
+    <h2>Repeat New Password</h2>
+    <input type="password" name="Password2" placeholder="">
+    <h2>Confirm With Old Password</h2>
+    <input type="password" name="CurrentPassword" placeholder="" required>
+    <input type="password" name="userID" value="<?php echo $userID;?>" placeholder="" hidden>
+    </br>
+    <input type="submit" name="" value="Update">
+    </form>
+  </div>
  </body>
   </html>
 <?php
