8a74a7bf5ebb3927504b4cccd2d46f2e7c69e6d9
mikaelpaavilainen <mikael.paavilainen@elev.it-gymnasiet.se>
2017-10-30 20:42:25 +0100

    clean

--- helpers/db.php
+++
@@ -1,9 +0,0 @@
-<!--Connects to database for info-->
-<?php
-$dsn = "mysql:host=localhost;dbname=thundrpuffin"; //Server Auth
-$login = "thundrpuffin";//Username
-$password = "t1";//Password
-$options = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"); //Set Value as UT8 Coding
-$dbh = new pdo($dsn,$login,$password,$options); //Establish Con..
-$dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-?>
--- helpers/popup.js
+++
@@ -1,5 +0,0 @@
- function popup(popupid) {
-   //alert(popupid)
-    var popup = document.getElementById(popupid);
-    popup.classList.toggle("show");
-}
