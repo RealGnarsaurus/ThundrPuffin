787382e41d42a3da8a8b49741f1b443e1986010f
MikaelP <mikael.paavilainen@elev.it-gymnasiet.se>
2017-10-30 10:23:51 +0100

    reupload

---
+++ helpers/eula.php
@@ -0,0 +1,55 @@
+<style>
+h2{
+  color:red;
+  font-size: 40px;
+}
+body{
+
+  background-repeat: no-repeat;
+}
+#Terms{
+  width: 100%;
+  margin: auto;
+  text-align: center;
+}
+#Terms li, ul{
+  margin: auto;
+  text-align: left;
+  font-size: 25px;
+  padding: 10px;
+}
+</style>
+<!DOCTYPE html>
+<html>
+<title>Eula</title>
+<head></head>
+<body>
+  <div id="Terms">
+  <h2>Terms & Conditions</h2>
+<ol>
+  <li>  The intellectual property <b>”ThundrPuffin”</b> rightfully <b>belongs</b> to the <b>ThundrPuffin Studio.</b> Unauthorized Use of its <b>logo and name</b> is <b>strictly prohibited</b> unless <b>specific permission</b> is granted by the <b>ThundrPuffin studio.</b></li>
+  <br>
+  <li>  The <b>ThundrPuffin studio</b> takes <b>NO responsibility</b> for <b>YOUR</b> or <b>ANY</b> other users actions.</li>
+<br>
+  <li>  If <b>need be,</b> the <b>ThundrPuffin studios can</b> and <b>WILL</b> give out <b>personal information</b> to <b>aid</b> any and all <b>investigations</b> conducted by <b>legitimate authorities</b> with <b>legitimate proxies.</b></li>
+<br>
+  <li>  By <b><i>accepting</i></b> these <b>terms and conditions</b> you <b><i>agree</i> to have some of your personal information <i>logged</i></b>, <b>including but not limited to:</b></li>
+  <ul>
+    <b>
+    <li>	ThundrPuffin Username</li>
+    <li>	ThundrPuffin Password</li>
+    <li>	Email</li>
+    <li>	Public IP</li>
+    <li>	Private IP</li>
+  </b>
+  </ul>
+  <br>
+  <li>  As the <b>rightful</b> owners of the intellectual property <b>”ThundrPuffin”</b>, we <b>CAN</b> and <b>WILL</b> outright <b>TERMINATE</b> user <b>accounts</b> and/or <b>blogs</b> if we deem it <b>necessary.</b></li>
+<br>
+  <li>  <b>Failure</b> to <b>comply</b> by or by all means <b>ignore</b> these rules will warrant <b>monetary compensation</b> to the <b>ThundrPuffin studios</b> relative to the <b>damage caused</b> to the intellectual property.</li>
+  <br>
+  <li>  In the event of <b><i>malicious</i> action</b>, <b>intended</b> or <b>nonintended</b>, the <b>user/users</b> that caused the event <b>CAN</b> and <b>WILL</b> <b><i>suffer</i></b> legal action to the fullest extent of the <b>law</b>.
+</ol>
+</div>
+</body>
+</html>
---
+++ helpers/getcontent.js
@@ -0,0 +1,106 @@
+//Opens Manage User
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("content").innerHTML =
+        this.responseText;
+      }
+    };
+    xhttp.open("GET", "../permission.php", true);
+    xhttp.send();
+  }
+  //Opens My Profile
+  function MyProfile() {
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          document.getElementById("content").innerHTML =
+          this.responseText;
+        }
+      };
+      xhttp.open("GET", "../myprofile.php", true);
+      xhttp.send();
+    }
+  //opens Custommize Blogg
+function blogSettings() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("content").innerHTML =
+            this.responseText;
+        }
+    };
+    xhttp.open("GET", "../manageblogg.php", true);
+    xhttp.send();
+    document.getElementById("content").style.height = "auto";
+}
+//open flagreports
+function showFlagReports() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("content").innerHTML =
+            this.responseText;
+        }
+    };
+    xhttp.open("GET", "../reporthistory.php", true);
+    xhttp.send();
+}
+//open delete history
+function showDelete() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("content").innerHTML =
+            this.responseText;
+        }
+    };
+    xhttp.open("GET", "../deletehistory.php", true);
+    xhttp.send();
+}
+//open Eula
+function eula() {
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          myWindow = window.open("helpers/eula.php", "_blank", "toolbar=yes,scrollbars=yes,resizable=no,top=200,left=300,width=1210,height=620");
+          popupBlockerChecker.check(myWindow);
+          myWindow.document.write(this.responseText);
+
+        }
+    };
+    xhttp.open("GET", "helpers/eula.php", true);
+    xhttp.send();
+
+}
+function popup(popupid) {
+  //alert(popupid)
+   var popup = document.getElementById(popupid);
+   popup.classList.toggle("show");
+}
+var popupBlockerChecker = {
+        check: function(popup_window){
+            var _scope = this;
+            if (popup_window) {
+                if(/chrome/.test(navigator.userAgent.toLowerCase())){
+                    setTimeout(function () {
+                        _scope._is_popup_blocked(_scope, popup_window);
+                     },200);
+                }else{
+                    popup_window.onload = function () {
+                        _scope._is_popup_blocked(_scope, popup_window);
+                    };
+                }
+            }else{
+                _scope._displayError();
+            }
+        },
+        _is_popup_blocked: function(scope, popup_window){
+            if ((popup_window.innerHeight > 0)==false){ scope._displayError(); }
+        },
+        _displayError: function(){
+            alert("Popup Blocker is Enabled! Please Add This Site To Your Exception List\n Or Agree In Blindness.");
+        }
+    };
